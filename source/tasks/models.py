19c9a944ddbc1d40a4cd4f0f39ad60280d740aff
Beka <>
2020-02-10 15:33:28 +0600

    Created models Task and Tag

--- source/tasks/models.py
+++ source/tasks/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Name')
+    description = models.TextField(max_length=1000, null=False, blank=False, verbose_name='Description')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Name')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+
+    def __str__(self):
+        return self.name
